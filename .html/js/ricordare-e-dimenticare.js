5219f6d0a3fe35232206e36a4f3c3722144bb802
Dave Nicolette <davenicolette@gmail.com>
2019-08-12 16:10:24 +0200

    Added more common phrases

---
+++ .html/js/ricordare-e-dimenticare.js
@@ -0,0 +1,9 @@
+var title = "Ricordare e dimenticare"
+
+var cards = []
+//cards[cards.length] = new card("english","italiano","audio.mp3")
+cards[cards.length] = new card("","",".mp3")
+cards[cards.length] = new card("","",".mp3")
+cards[cards.length] = new card("","",".mp3")
+cards[cards.length] = new card("","",".mp3")
+cards[cards.length] = new card("","",".mp3")
