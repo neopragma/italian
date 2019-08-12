3c9124007a7f9595e7ec646dfca83293c0efb8c1
Dave Nicolette <davenicolette@gmail.com>
2019-08-12 14:59:23 +0200

    Added a section for common phrases

---
+++ .html/js/frasi-comuni.js
@@ -0,0 +1,16 @@
+var title = "Modello per Schede Flash"
+
+var cards = []
+//cards[cards.length] = new card("english","italiano","audio.mp3")
+cards[cards.length] = new card("okay","va bene","va-bene.mp3")
+cards[cards.length] = new card("sorry, excuse me","scusa","scusa.mp3")
+cards[cards.length] = new card("please","per favore","per-favore.mp3")
+cards[cards.length] = new card("I didn't understand that; I didn't get it","Non l'ho capito","non-l-ho-capito.mp3")
+cards[cards.length] = new card("Repeat, please","Ripeteresti, per favore","ripeteresti-per-favore.mp3")
+cards[cards.length] = new card("A little slower, please","Un po 'pi&ugrave; lento, per favore","un-po-piu-lento-per-favore.mp3")
+cards[cards.length] = new card("Sorry, I don't speak Italian","Scusa, non parlo italiano","scusa-non-parlo-italiano.mp3")
+cards[cards.length] = new card("","",".mp3")
+cards[cards.length] = new card("","",".mp3")
+cards[cards.length] = new card("","",".mp3")
+cards[cards.length] = new card("","",".mp3")
+cards[cards.length] = new card("","",".mp3")
