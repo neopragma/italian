8d2da27eeb7aeab370e9563ff33cf6162d3f7efb
Dave Nicolette <davenicolette@gmail.com>
2019-08-12 14:46:23 +0200

    Added verb practice sections for mangiare presente and passato prossimo

--- .html/js/giorni-e-mesi.js
+++ .html/js/giorni-e-mesi.js
@@ -1,4 +1,4 @@
-var title = "Giorni e Mesi"
+var title = "Giorni e mesi"
 
 var cards = []
 cards[cards.length] = new card("January","gennaio","gennaio.mp3")
@@ -40,25 +40,6 @@ cards[cards.length] = new card("February has 28 days except in leap years, when 
 cards[cards.length] = new card("a year has 365 days, but a leap year has 366","un anno ha trecentosessantacinque giorni, ma un anno bisestile ha trecentosessantasei","un-anno-ha-365-giorni.mp3")
 cards[cards.length] = new card("payment is due on the fifteenth of every month","il pagamento &egrave; dovuto il quindici di ogni mese","il-pagamento-e-dovuto.mp3")
 cards[cards.length] = new card("Beware the Ides of March!","Attenzione alle idi di marzo!","idi-di-marzo.mp3")
-cards[cards.length] = new card("winter","inverno","inverno.mp3")
-cards[cards.length] = new card("spring","primavera","primavera.mp3")
-cards[cards.length] = new card("summer","estate","estate.mp3")
-cards[cards.length] = new card("autumn (fall)","autunno","autunno.mp3")
-cards[cards.length] = new card("the Winter Solstice","il solstizio d'inverno","il-solstizio-d-inverno.mp3")
-cards[cards.length] = new card("winter begins around December 21st or 22nd in the northern hemisphere","l'inverno inizia intorno al ventunesimo o ventiduesimo dicembre nell'emisfero settentrionale","l-inverno-inizia.mp3")
-cards[cards.length] = new card("At the winter solstice, the Sun's path has reached its southernmost position. The next day, the path will advance northward.","Al solstizio d'inverno, il percorso del Sole ha raggiunto la sua posizione pi&ugrave; meridionale. Il giorno successivo, il percorso avanzer&agrave; verso nord.","il-solstizio-d-inverno-2.mp3")
-cards[cards.length] = new card("The day of the winter solstice is the shortest day of the year","Il giorno del solstizio d'inverno &egrave; il giorno pi&ugrave; corto dell'anno","il-solstizio-d-inverno-3.mp3")
-cards[cards.length] = new card("the Summer Solstice","il solstizio d'estate","il-solstizio-d-estate.mp3")
-cards[cards.length] = new card("summer begins around June 20th or 21st in the northern hemisphere","l'estate inizia intorno al 20 o 21 giugno nell'emisfero settentrionale","l-estate-inizia.mp3")
-cards[cards.length] = new card("the Summer Solstice occurs when the Sun reaches both its highest and northernmost points in the sky.","il solstizio d'estate si verifica quando il Sole raggiunge sia i suoi punti pi&ugrave; alti che quelli pi&ugrave; settentrionali nel cielo.","il-solstizio-d-estate-2.mp3")
-cards[cards.length] = new card("The day of the Summer Solstice is the longest day of the year","Il giorno del solstizio d'estate &egrave; il giorno pi&ugrave; lungo dell'anno","il-solstizio-d-estate-3.mp3")
-cards[cards.length] = new card("the vernal equinox","l'equinozio di primavera","l-equinozio-di-primavera.mp3")
-cards[cards.length] = new card("the vernal equinox always occurs on March 19, 20, or 21.","l'equinozio di primavera si verifica sempre il 19, 20 o 21 marzo.","l-equinozio-di-primavera-2.mp3")
-cards[cards.length] = new card("the vernal equinox marks the astronomical first day of spring in the Northern Hemisphere","l'equinozio di primavera segna il primo giorno astronomico di primavera nell'emisfero settentrionale","l-equinozio-di-primavera-3.mp3")
-cards[cards.length] = new card("the autumnal equinox","l'equinozio autunnale","l-equinozio-autunnale.mp3")
-cards[cards.length] = new card("the autumnal equinox always occurs around September 22 or 23","l'equinozio autunnale si verifica sempre intorno al 22 o 23 settembre","l-equinozio-autunnale-2.mp3")
-cards[cards.length] = new card("the autumnal equinox marks the start of autumn in the Northern Hemisphere and spring in the Southern Hemisphere.","l'equinozio autunnale segna l'inizio dell'autunno nell'emisfero settentrionale e la primavera nell'emisfero australe.","l-equinozio-autunnale-3.mp3")
-cards[cards.length] = new card("at the vernal and autumnal equinoxes, day and night are of equal length","agli equinozi di primavera e di autunno, il giorno e la notte sono di uguale lunghezza","equinozi.mp3")
 cards[cards.length] = new card("there are twelve months in a year","ci sono dodici mesi in un anno","dodici-mesi-in-un-anno.mp3")
 cards[cards.length] = new card("there are seven days in a week","ci sono sette giorni in una settimana","sette-giorni-in-una-settimana.mp3")
 cards[cards.length] = new card("daylight","luce del giorno","luce-del-giorno.mp3")
@@ -71,5 +52,4 @@ cards[cards.length] = new card("morning","la mattina","la-mattina.mp3")
 cards[cards.length] = new card("afternoon","il pomeriggio","il-pomeriggio.mp3")
 cards[cards.length] = new card("evening","la sera","la-sera.mp3")
 cards[cards.length] = new card("night","la notte","la-notte.mp3")
-cards[cards.length] = new card("winter is coming","l'inverno sta arrivando","l-inverno-sta-arrivando.mp3")
 
---
+++ .html/js/parlando-di-cibo.js
@@ -0,0 +1,12 @@
+var title = "Parlando di cibo"
+
+var cards = []
+//cards[cards.length] = new card("english","italiano","audio.mp3")
+cards[cards.length] = new card("What's for lunch today?","Cosa c'&egrave; per pranzo oggi?","cosa-ce-per-pranzo-oggi.mp3")
+cards[cards.length] = new card("Who says ice cream isn't for breakfast?","Chi dice che il gelato non &egrave; per colazione?","chi-dice-che-il-gelato-non-e-per-colazione.mp3")
+cards[cards.length] = new card("","",".mp3")
+cards[cards.length] = new card("","",".mp3")
+cards[cards.length] = new card("","",".mp3")
+cards[cards.length] = new card("","",".mp3")
+cards[cards.length] = new card("","",".mp3")
+cards[cards.length] = new card("","",".mp3")
